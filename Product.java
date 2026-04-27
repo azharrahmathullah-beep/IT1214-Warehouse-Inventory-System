57ad4c6626cfd4e24d12bbe3ebdc17cc9dbbc033
azharrahmathullah-beep <azharrahmathullah@gmail.com>
2026-04-27 21:04:25 +0530

    Product.java

--- Product.java
+++ Product.java
@@ -1,9 +1,9 @@
 public class Product {
-    private int id;
-    private String name;
-    private String category;
-    private int quantity;
-    private double price;
+     int id;
+     String name;
+     String category;
+     int quantity;
+     double price;
 
     public Product(int id, String name, String category, int quantity, double price) {
         this.id = id;
